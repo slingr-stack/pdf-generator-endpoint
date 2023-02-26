b5ae18f43ae96f24f6b7075e2e99b1f184b4c69c
Diego Gaviola <dgaviola@slingr.io>
2023-02-26 18:53:29 -0300

    add support to draw images in the full page

--- src/main/java/io/slingr/endpoints/pdfGenerator/workers/AddImagesWorker.java
+++ src/main/java/io/slingr/endpoints/pdfGenerator/workers/AddImagesWorker.java
@@ -10,6 +10,7 @@ import io.slingr.endpoints.ws.exchange.FunctionRequest;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -74,12 +75,26 @@ public class AddImagesWorker extends PdfImageWorker {
                             PDImageXObject pdImage = PDImageXObject.createFromFileByContent(img, pdf);
                             PDPageContentStream contentStream = new PDPageContentStream(pdf, page, PDPageContentStream.AppendMode.APPEND, true);
 
-                            int x = image.contains("x") ? image.integer("x") : 20;
-                            int y = image.contains("y") ? image.integer("y") : 20;
-                            int width = image.contains("width") ? image.integer("width") : 100;
-                            int height = image.contains("height") ? image.integer("height") : 100;
-
-                            contentStream.drawImage(pdImage, x, y, width, height);
+                            if (image.contains("fullPage") && image.bool("fullPage")) {
+                                PDRectangle mediaBox = page.getMediaBox();
+                                float pageWidth = mediaBox.getWidth();
+                                float pageHeight = mediaBox.getHeight();
+                                // calculate the scaling factor to make the image take up the entire page space
+                                float scaleX = pageWidth / pdImage.getWidth();
+                                float scaleY = pageHeight / pdImage.getHeight();
+                                float scale = Math.max(scaleX, scaleY);
+                                // calculate the position of the image on the top left corner of the page
+                                float x = 0;
+                                float y = pageHeight - (pdImage.getHeight() * scale);
+                                // create a new content stream and draw the image
+                                contentStream.drawImage(pdImage, x, y, pdImage.getWidth() * scale, pdImage.getHeight() * scale);
+                            } else {
+                                int x = image.contains("x") ? image.integer("x") : 20;
+                                int y = image.contains("y") ? image.integer("y") : 20;
+                                int width = image.contains("width") ? image.integer("width") : 100;
+                                int height = image.contains("height") ? image.integer("height") : 100;
+                                contentStream.drawImage(pdImage, x, y, width, height);
+                            }
                             contentStream.close();
 
                         }
